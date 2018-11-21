2c5635a7b40e327c6cdd2a71a0b5620e8f29ee0e
Hiroto Nagai <naga.2431610@gmail.com>
2018-11-22 02:29:19 +0900

    2ndbattleの追加＆メンバー紹介更新

---
+++ mita2018/script/dush2.js
@@ -0,0 +1,144 @@
+for(var k=0; k<=7;k++){
+  var ttt = document.getElementsByClassName("answer-box")[k]
+if(k<4){
+  ttt.style.top=210+"px";
+  ttt.style.left=(127+k*215)+"px";
+}else{
+    ttt.style.top=527+"px";
+  ttt.style.left=(127+(k-4)*215)+"px";
+  }
+}
+(function(){
+
+    //要素の取得
+    var elements = document.getElementsByClassName("drag-card");
+    //要素内のクリックされた位置を取得するグローバル（のような）変数
+    var x;
+    var y;
+
+    //マウスが要素内で押されたとき、又はタッチされたとき発火
+    for(var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("mousedown", mdown, false);
+        elements[i].addEventListener("touchstart", mdown, false);
+    }
+
+    //マウスが押された際の関数
+    function mdown(e) {
+
+        //クラス名に .drag を追加
+        this.classList.add("drag");
+
+        //タッチデイベントとマウスのイベントの差異を吸収
+        if(e.type === "mousedown") {
+            var event = e;
+        } else {
+            var event = e.changedTouches[0];
+        }
+
+        //要素内の相対座標を取得
+        x = event.pageX - this.offsetLeft;
+        y = event.pageY - this.offsetTop;
+
+        //ムーブイベントにコールバック
+        document.body.addEventListener("mousemove", mmove, false);
+        document.body.addEventListener("touchmove", mmove, false);
+    }
+
+    //マウスカーソルが動いたときに発火
+    function mmove(e) {
+
+        //ドラッグしている要素を取得
+        var drag = document.getElementsByClassName("drag")[0];
+
+        //同様にマウスとタッチの差異を吸収
+        if(e.type === "mousemove") {
+            var event = e;
+        } else {
+            var event = e.changedTouches[0];
+        }
+
+        //フリックしたときに画面を動かさないようにデフォルト動作を抑制
+        e.preventDefault();
+
+        //マウスが動いた場所に要素を動かす
+        drag.style.top = event.pageY - y + "px";
+        drag.style.left = event.pageX - x + "px";
+
+        var flipval = drag.id+"_flip";
+        var flipid = document.getElementById(flipval);
+
+        flipid.style.top = event.pageY - y+235 + "px";
+        flipid.style.left = event.pageX - x+133 + "px";
+
+        //マウスボタンが離されたとき、またはカーソルが外れたとき発火
+        drag.addEventListener("mouseup", mup, false);
+        document.body.addEventListener("mouseleave", mup, false);
+        drag.addEventListener("touchend", mup, false);
+        document.body.addEventListener("touchleave", mup, false);
+
+    }
+
+    //マウスボタンが上がったら発火
+    function mup(e) {
+        var drag = document.getElementsByClassName("drag")[0];
+        //ムーブベントハンドラの消去
+        document.body.removeEventListener("mousemove", mmove, false);
+        drag.removeEventListener("mouseup", mup, false);
+        document.body.removeEventListener("touchmove", mmove, false);
+        drag.removeEventListener("touchend", mup, false);
+        checkbox();
+        //クラス名 .drag も消す
+        drag.classList.remove("drag");
+
+    }
+
+function checkbox(){
+          var drag = document.getElementsByClassName("drag")[0];
+          var top = parseFloat(drag.style.top)
+          var left = parseFloat(drag.style.left)
+
+for(var i =0; i<=7;i++){
+  var fff = document.getElementsByClassName("answer-box")[i]
+  if(top-parseFloat(fff.style.top)<=50 && top - parseFloat(fff.style.top) >= -50 && left-parseFloat(fff.style.left)<=30 && left-parseFloat(fff.style.left)>=-30){
+
+    drag.style.top=(parseFloat(fff.style.top)+3)+"px";
+    drag.style.left=(parseFloat(fff.style.left)+3)+"px";
+    var flipval = drag.id+"_flip";
+    var flipid = document.getElementById(flipval);
+    flipid.style.top = (parseFloat(fff.style.top)+238)+"px";
+    flipid.style.left = (parseFloat(fff.style.left)+136)+"px";
+
+  }
+}
+
+checkanswer();
+}
+function checkanswer(){
+  var getdrag=document.getElementsByClassName("drag-card");
+	  var getbox=document.getElementsByClassName("answer-box");
+  var top=[];
+  var left=[];
+	var top1=[];
+	var left1=[];
+	var ansssss=0;
+  for(var i = 0; i<getdrag.length; i++){
+    top[i] = parseFloat(getdrag[i].style.top)
+    left[i] = parseFloat(getdrag[i].style.left)
+		top1[i] = parseFloat(getbox[i].style.top)+3
+		left1[i] = parseFloat(getbox[i].style.left)+3
+  }
+	for(var ii=0; ii<=7; ii++){
+		if(top[ii]==top1[ii] && left[ii]==left1[ii]){
+			ansssss++;
+		}
+	}
+  if(ansssss==8){
+    var flipside=document.getElementsByClassName("flipbutton")
+    if(flipside[0].innerHTML=="裏" && flipside[1].innerHTML=="裏" && flipside[2].innerHTML=="裏" && flipside[3].innerHTML=="裏" && flipside[4].innerHTML=="表" && flipside[5].innerHTML=="表" &&flipside[6].innerHTML=="表" && flipside[7].innerHTML=="表"){
+    location.href="clear_page.html"
+}
+}
+}
+
+
+})()
---
+++ mita2018/script/dush2_1.js
@@ -0,0 +1,136 @@
+
+
+
+var setting;
+
+window.onload = function(){
+
+	setting = [
+		{
+			form: document.getElementById('form'),
+			button: document.getElementById('button'),
+			message: document.getElementById('message'),
+			folder: 'answer',
+			sending: false,
+		},
+	]
+
+	for(var i = 0; i < setting.length; ++i) {
+		if( setting[i]['button'] ) {
+			setting[i]['button'].onclick = (function(_i){return function(){ checkAns(_i); };})(i);
+		}
+		if( setting[i]['form'] ) {
+			setting[i]['form'].onkeydown = (function(_i){ return function(event){
+				if (event == undefined) event = window.event;
+				if (event.keyCode == 13) checkAns(_i);
+			}})(i);
+		}
+	}
+
+}
+
+function checkAns(id){
+
+	if(setting[id]['sending']) return;
+	setting[id]['sending'] = true;
+	setting[id]['button'].disabled = true;
+	setting[id]['message'].innerText = "判定中...";
+
+	var request = new XMLHttpRequest();
+	request.timeout = 10000;
+	request.open('GET', setting[id]['folder'] + '/' + encodeURIComponent(setting[id]['form'].value));
+	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	request.send(null);
+
+	request.onreadystatechange = function(){
+		if(request.readyState == 4){
+			if( setting[id]['form'].value == '' ){
+				setting[id]['message'].innerText = "不正解です";
+			} else {
+				switch(request.status){
+					case 200:
+            var ttt=Number(request.responseText);
+						mission(ttt);
+            setting[id]['message'].innerText = "";
+						break;
+					case 404:
+						setting[id]['message'].innerText = "不正解です";
+						break;
+					default:
+						setting[id]['message'].innerText = "不正解です";
+						break;
+				}
+			}
+			setting[id]['button'].disabled = false;
+			setting[id]['sending'] = false;
+		}
+	}
+
+	request.ontimeout = function(){
+		setting[id]['message'].innerText = "タイムアウトしました。もう一度やりなおしてください。";
+		setting[id]['button'].disabled = false;
+		setting[id]['sending'] = false;
+	}
+
+}
+
+
+function mission(n){
+  switch(n){
+    case 0:
+    document.getElementById("mis1").innerHTML='<img src="image/mission1_2.png" class="missions">'
+    break;
+    case 1:
+    document.getElementById("mis2").innerHTML='<img src="image/mission2_2.png" class="missions">'
+    break;
+    case 2:
+    document.getElementById("mis3").innerHTML='<img src="image/mission3_2.png" class="missions">'
+    break;
+    case 3:
+    document.getElementById("mis4").innerHTML='<img src="image/mission4_2.png" class="missions">'
+    break;
+  }
+}
+
+function flipcard(n){
+
+var getflip=document.getElementsByClassName("drag-card")[n];
+var getflips=document.getElementsByClassName("flipbutton")[n];
+
+
+if(getflips.innerHTML=="表"){
+  getflip.style.backgroundImage="url(image/card"+(n+1)+".png)";
+  getflips.innerHTML="裏"
+}else{getflip.style.backgroundImage="url(image/card"+(n+1)+"_ura.png)";
+  getflips.innerHTML="表"
+}
+checkanswer();
+}
+
+
+function checkanswer(){
+  var getdrag=document.getElementsByClassName("drag-card");
+	  var getbox=document.getElementsByClassName("answer-box");
+  var top=[];
+  var left=[];
+	var top1=[];
+	var left1=[];
+	var ansssss=0;
+  for(var i = 0; i<getdrag.length; i++){
+    top[i] = parseFloat(getdrag[i].style.top)
+    left[i] = parseFloat(getdrag[i].style.left)
+		top1[i] = parseFloat(getbox[i].style.top)+3
+		left1[i] = parseFloat(getbox[i].style.left)+3
+  }
+	for(var ii=0; ii<=7; ii++){
+		if(top[ii]==top1[ii] && left[ii]==left1[ii]){
+			ansssss++;
+		}
+	}
+  if(ansssss==8){
+    var flipside=document.getElementsByClassName("flipbutton")
+    if(flipside[0].innerHTML=="裏" && flipside[1].innerHTML=="裏" && flipside[2].innerHTML=="裏" && flipside[3].innerHTML=="裏" && flipside[4].innerHTML=="表" && flipside[5].innerHTML=="表" &&flipside[6].innerHTML=="表" && flipside[7].innerHTML=="表"){
+    location.href="index.html"
+}
+}
+}
