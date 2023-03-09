8e852f881d1436adfacf49089b9c8756e6066bcf
SAKI FUJIMURA <69893801+Sakippo@users.noreply.github.com>
2023-03-09 17:35:58 +0900

    Update login.js

--- secret/js/login.js
+++ secret/js/login.js
@@ -4,11 +4,11 @@ function checkLogin(){
     const password = document.querySelector('#password').value;
 
     if (mail == "knurt@ponko2.com" && password == "イチゴ"){
-        window.location.href = "../trunk.html";
+        window.location.href = "./trunk.html";
     }else if(mail == "deb@ponko2.com" && password == "ナシ"){
-        window.location.href = "../bed.html";
+        window.location.href = "./bed.html";
     }else if(mail == "niarb@ponko2.com" && password == "ナシ"){
-        window.location.href = "../brain.html";
+        window.location.href = "./brain.html";
     }else{
         window.alert("メールアドレスまたはパスワードが間違っています。");
     }
