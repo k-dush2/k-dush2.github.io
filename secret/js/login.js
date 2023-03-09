45e90f360342c469de1f291c11b523cb1ccd8283
SAKI FUJIMURA <69893801+Sakippo@users.noreply.github.com>
2023-03-09 16:45:01 +0900

    Add files via upload

---
+++ secret/js/login.js
@@ -0,0 +1,16 @@
+function checkLogin(){
+
+    const mail = document.querySelector('#mail').value;
+    const password = document.querySelector('#password').value;
+
+    if (mail == "knurt@ponko2.com" && password == "イチゴ"){
+        window.location.href = "../trunk.html";
+    }else if(mail == "deb@ponko2.com" && password == "ナシ"){
+        window.location.href = "../bed.html";
+    }else if(mail == "niarb@ponko2.com" && password == "ナシ"){
+        window.location.href = "../brain.html";
+    }else{
+        alart("メールアドレスまたはパスワードが間違っています。");
+    }
+
+};
